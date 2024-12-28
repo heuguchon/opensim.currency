05bc3d5de7ec3e5a2db201599a74165117849fda
흑우촌 <89528038+heuguchon@users.noreply.github.com>
2024-12-29 00:14:21 +0900

    chore: Upgrade dotnet version in runprebuild

--- runprebuild.net.sh
+++ runprebuild.net.sh
@@ -2,5 +2,5 @@
 #
 echo y | dotnet ../bin/prebuild.dll /file prebuild.net.xml /clean
 
-dotnet ../bin/prebuild.dll /target vs2022 /targetframework net6_0 /excludedir = "obj | bin" /file prebuild.net.xml
+dotnet ../bin/prebuild.dll /target vs2022 /targetframework net8_0 /excludedir = "obj | bin" /file prebuild.net.xml
 
